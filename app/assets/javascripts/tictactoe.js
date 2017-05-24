aa0e518cedafa167c7ee7c62201a73b9f7a43d3f
Chuan-Sheng Tai <Johnson@ChuanShengsAir.domain>
2017-05-24 01:25:01 -0700

    make the game work

---
+++ app/assets/javascripts/tictactoe.js
@@ -0,0 +1,46 @@
+var turn = 0;
+var winningCombo = [[[0,0],[1,0],[2,0]], [[0,1],[1,1],[2,1]], [[0,2],[1,2],[2,2]], [[0,0],[1,1],[2,2]], [[0,0],[0,1],[0,2]], [[2,0],[2,1],[2,2]], [[1,0],[1,1],[1,2]], [[2,0],[1,1],[0,2]]]
+
+function attachListeners(){
+	$("td").on("click", function(){
+		doTurn(this)
+	})
+}
+
+function doTurn(box){
+	turn += 1;
+	updateState(box);
+	checkWinner();
+}
+
+function doTurn(box){
+	turn += 1;
+	updateState(box);
+	checkWinner();
+}
+
+function player(){
+	if(turn % 2 === 1){
+		return "O"
+	}else {
+		return "X"
+	}
+}
+
+function updateState(box){
+	var token = player();
+	$(box).text(token);
+}
+
+function checkWinner(){
+	winningCombo.forEach(array => {
+		if(boxSelector(array[0]) === boxSelector(array[1]) && boxSelector(array[1]) === boxSelector(array[2]) && boxSelector(array[0]) != ""){
+			var messageStr = "Player " + boxSelector(array[0]) + " Won!"
+			message(messageStr);
+		}
+	})
+}
+
+function message(str){
+	$("#message").append("<p>" + str + "</p>")
+}
