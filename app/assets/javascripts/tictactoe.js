bb6d6a127f6a6542cfa2110ff9ae38386adc4eb3
tahbristol <tahbristol@gmail.com>
2017-12-14 16:40:53 -0500

    checkWinner in progress

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,5 @@
 // Code your JavaScript / jQuery solution here
-
+const WIN_COMBINATIONS =  [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 var turn = 0;
 function player(){
   if (turn % 2 === 0){
@@ -18,3 +18,16 @@ function setMessage(message){
   let messageElement = document.getElementById('message');
   messageElement.innerHTML = message;
 }
+
+function checkWinner(){
+  WIN_COMBINATIONS.forEach(function(combo){
+    let one = combo[1];
+    let two = combo[2];
+    let three = combo[3];
+
+
+
+
+  })
+}
+var td = document.getElementsByTagName('td');
