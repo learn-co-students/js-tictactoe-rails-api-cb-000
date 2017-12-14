8a6480b342ddd318d65a7cc4a7cab78c86c6de12
tahbristol <tahbristol@gmail.com>
2017-12-14 16:16:15 -0500

    10 tests passing

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,20 @@
 // Code your JavaScript / jQuery solution here
+
+var turn = 0;
+function player(){
+  if (turn % 2 === 0){
+    return "X";
+  }else{
+    return "O";
+  }
+}
+
+function updateState(position){
+  let token = player();
+  position.innerHTML = token;
+}
+
+function setMessage(message){
+  let messageElement = document.getElementById('message');
+  messageElement.innerHTML = message;
+}
