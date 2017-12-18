bb8e2149b2fd45ae92d14c710483caa97397cc5c
tahbristol <tahbristol@gmail.com>
2017-12-17 22:55:27 -0500

    19 tests passing

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,33 +1,158 @@
 // Code your JavaScript / jQuery solution here
-const WIN_COMBINATIONS =  [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-var turn = 0;
-function player(){
-  if (turn % 2 === 0){
-    return "X";
-  }else{
-    return "O";
+
+$(document).ready(function(){
+
+
+ //checkWinner();
+ attachListeners();
+});
+
+
+
+  var turn = 0;
+  function player(){
+    if (turn % 2 === 0){
+      return "X";
+    }else{
+      return "O";
+    }
+  }
+function attachListeners(){
+  let board = getBoard();
+  var saveBtn = document.getElementById('save');
+    saveBtn.addEventListener("click", function(event){
+        let gameBoard = `{"state": "${board}"}`
+        let savedGame = $.post('/games', JSON.parse(gameBoard));
+        savedGame.done(function(response){
+          console.log(response);
+        });
+    })
+  var previousBtn = document.getElementById('previous');
+    previousBtn.addEventListener("click", function(event){
+       fetch('/games')
+       .then(res => res.json())
+       .then(games => {
+         let gamesDiv = document.getElementById('games');
+           games.data.forEach(function(g){
+             gamesDiv.innerHTML += `<button onclick="loadGame(${g.id})" >Game :${g.id} </button>`
+           })
+
+
+
+       });
+
+    })
+  var clearBtn = document.getElementById('clear');
+   clearBtn.addEventListener("click", function(event){
+     alert("clicked")
+      let board = [" "," "," "," "," "," "," "," "," "];
+      let gameBoard = `{"state": "${board}"}`
+      let saveGame = $.post('/games', JSON.parse(gameBoard));
+      saveGame.done(function(response){
+        fillBoard(response.data.attributes.state);
+      });
+   });
+
+   var spaces = document.getElementsByTagName('td');
+    for(let i = 0; i < 9; i++){
+      spaces[i].addEventListener("click", function(e){
+
+        if(this.innerHTML === ""){
+          doTurn(this);
+        }
+
+      });
+    }
+
+
+
+}
+
+function loadGame(game){
+  console.log(game);
+  let savedGame = $.get(`/games/${game}`, JSON.parse(game));
+  savedGame.done(function(response){
+  fillBoard(response.data.attributes.state);
+  });
+}
+
+function fillBoard(game = ["","","","","","","","",""]){
+  console.log(game);
+     let td = document.getElementsByTagName('td');
+      for(let i = 0; i < 9; i++){
+        if(game[i] === undefined){
+          td[i].innerHTML = "";
+        }else{
+          td[i].innerHTML = game[i];
+
+        }
+      }
+}
+
+function doTurn(clickedSpace){
+  turn++;
+  updateState(clickedSpace);
+  if(checkWinner() === true){
+    fillBoard();
+    turn = 0;
   }
 }
+  function updateState(position){
+    let token = player();
+    position.innerHTML = token;
+  }
 
-function updateState(position){
-  let token = player();
-  position.innerHTML = token;
-}
+  function setMessage(message){
+    let messageElement = document.getElementById('message');
+    messageElement.innerHTML = message;
+  }
 
-function setMessage(message){
-  let messageElement = document.getElementById('message');
-  messageElement.innerHTML = message;
-}
+  function checkWinner(){
+    let board = getBoard();
 
-function checkWinner(){
-  WIN_COMBINATIONS.forEach(function(combo){
-    let one = combo[1];
-    let two = combo[2];
-    let three = combo[3];
+console.log(board);
+
+    let winner = false;
+    const WIN_COMBINATIONS =  [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
 
+      WIN_COMBINATIONS.forEach(function(combo){
+        let index1 = combo[0];
+        let index2 = combo[1];
+        let index3 = combo[2];
+
+        let pos1 = board[index1];
+        let pos2 = board[index2];
+        let pos3 = board[index3];
+
+        if((pos1 === "X" && pos2 === "X" && pos3 === "X") || (pos1 === "O" && pos2 === "O" && pos3 === "O")){
+
+          return  winner = true;
+
+        }
 
 
-  })
-}
-var td = document.getElementsByTagName('td');
+
+
+
+  });
+   if(winner){
+     setMessage(`Player ${player()} Won!`);
+   }
+    return winner;
+
+  }
+
+  function getBoard(){
+    var board = [];
+     var tdTags = document.getElementsByTagName('td');
+     for(let i = 0; i < 9; i++){
+       if(tdTags[i].innerText !== ""){
+         board.push(tdTags[i].innerText);
+       }else{
+         board.push("");
+       }
+     }
+
+     return board;
+  }
