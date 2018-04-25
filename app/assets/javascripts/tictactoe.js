9feecf57b05672064f3265d29166b7256965aa9a
Heather Christman <efl7a@yahoo.de>
2018-04-25 18:23:01 -0400

    Passing some tests

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,10 +1,9 @@
 $(document).ready(attachListeners)
 
-let turn = 0
-let board = ["", "", "", "", "", "", "", "", ""]
+var turn = 0
 
 function player() {
-  return turn % 2 === 0 ? "X":"O"
+  return ((turn % 2 === 0) ? "X":"O")
 }
 
 function updateState() {
@@ -16,12 +15,33 @@ function setMessage(message) {
 }
 
 function checkWinner() {
-
+  var wins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  board = createBoardArray()
+  result =  wins.some(function (combo) {
+    if(board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]] && board[combo[0]] != ""){
+      setMessage(`Player ${player()} Won!`)
+      return true;
+    }
+  })
+  return result
 }
 
 function doTurn() {
   // checkWinner(), updateState(), increments turn, saves completed games
   updateState.bind(this)()
+  var result = checkWinner()
+  if(turn >= 8){
+    setMessage("Tie game.")
+  }
   turn ++
 }
 
@@ -55,4 +75,17 @@ function clearGame(e) {
 function loadGame(){
   let id = $(this).data("id")
   alert(`looking for ${id}`)
+}
+
+
+function createBoardArray() {
+  let count = 0
+  let board = []
+  for(let y = 0; y < 3; y++ ){
+    for(let x = 0; x < 3; x++){
+      board[count] = $(`[data-x="${x}"][data-y="${y}"]`).text()
+      count ++
+    }
+  }
+  return board
 }
