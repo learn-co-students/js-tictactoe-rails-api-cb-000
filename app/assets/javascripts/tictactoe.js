ea81a2b17f91c9917b051e9a864188bac8d6f14b
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-06-27 13:22:09 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,4 +1,14 @@
 // Code your JavaScript / jQuery solution here
+const WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+]
 
 var turn = 0;
 
@@ -18,8 +28,8 @@ function player() {
   return ((turn % 2 === 0) ? "X" : "O");
 }
 
-function updateState() {
-
+function updateState(box) {
+  $(box).text(player());
 }
 
 function setMessage(msg) {
