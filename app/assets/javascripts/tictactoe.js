9b537ff15bea7961ef464167d3c2f34afae04683
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-06-27 12:27:49 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,41 @@
 // Code your JavaScript / jQuery solution here
+
+var turn = 0;
+
+function saveGame() {
+  console.log("I am saveGame!");
+}
+
+function previousGame() {
+  console.log("I am previousGame!");
+}
+
+function clearGame() {
+  console.log("I am clearGame!");
+}
+
+function player() {
+  return ((turn % 2 === 0) ? "X" : "O");
+}
+
+function updateState() {
+
+}
+
+function setMessage(msg) {
+  $("#message").text(msg);
+}
+
+function checkWinner() {
+
+}
+
+function doTurn() {
+  turn++;
+  updateState(this);  //element that has been clicked!
+  checkWinner();
+}
+
+function attachListeners() {
+
+}
